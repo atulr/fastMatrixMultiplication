0ef50433a9b143f7ae2dd69587ca8c504cb1e001
atulr <atulrungta08@gmail.com>
2013-09-28 21:42:24 -0400

    correcting an error message in mkl matrix allocation step...

--- fastMatrixMultiplication/mklmatrixvector.cpp
+++ fastMatrixMultiplication/mklmatrixvector.cpp
@@ -7,7 +7,7 @@ void mklMatrixVector::incrementCount(){
 void mklMatrixVector::pushMatrix(MKL_Complex8 *m, int row, int col) {
 	matrices[count++] = (MKL_Complex8 *)mkl_malloc( row*col*sizeof( MKL_Complex8 ), 32 );
 	if(!matrices[count]) {
-		fprintf(stderr, "!!!! cuda memory allocation error\n");
+		fprintf(stderr, "!!!! mkl memory allocation error\n");
 	}
 	matrices[count] = m;
 }
